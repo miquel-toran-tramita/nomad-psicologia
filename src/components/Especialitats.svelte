143507762fe8e44f2616c56fa644c967c129fcd0
Scripted <miquel@tramita.info>
2024-10-12 00:16:19 +0200

    cosas

--- src/components/Especialitats.svelte
+++ src/components/Especialitats.svelte
@@ -5,49 +5,49 @@
 
   let especialitats = [
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Psicoteràpia individual per nens, adolescents i adults',
       description:
         'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Psicoteràpia de família i de parella',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        'Quants més siguem a terapia més recursos tindrem a disposició de la resolució dels problemes. En la majoria de les situacions la conflictivitat no només afecta a la persona amb el diagnòstic (‘pacient identificat’), sinó a tot el sistema relacional.',
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Psicoteràpia grupal',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        'El treball terapèutic dins d’un grup ens permet dur a terme el tractament en un espai segur on poder-nos sentir escoltats i acompanyats, des de l’empatia i el respecte mutu. Un espai en què qualsevol company és algú amb qui recolzar-nos i que ens pot aportar una visió nova que possibiliti el canvi i la millora individual.',
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Espais de pares',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        "Oferim un espai d’orientació per a mares i pares on poder reflexionar conjuntament sobre la paternitat, compartir emocions, trobar respostes i aportar pautes educacionals concretes, per tal d'obtenir més confiança i seguretat en les seves tasques parentals.",
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Espais de reflexió (Tallers i xerrades)',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        'Organitzem tallers i xerrades amb l’objectiu de generar espais de reflexió dinàmics i vivencials, a partir de les necessitats i demandes que ens arriben des de les escoles i els instituts.',
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Logopèdia',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        'A partir del disseny d’activitats per la prevenció, avaluació i tractament dels trastorns de la comunicació, el llenguatge, la parla, l’audició i les funcions orals no verbals, intentem millorar les capacitats comunicatives de cada persona.',
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Reeducació',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        "Concebem la reeducació pedagògica com un tractament psicològic dinàmic basat en una relació terapèutica en què el nen és part activa, amb l'objectiu de treballar les dificultats en els processos d’aprenentatge que afecten el rendiment acadèmic.",
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Assessorament a professionals',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        'El servei de formació i assessorament a professionals té la finalitat d’orientar i proporcionar recursos a professionals de diferents àmbits assistencials, a través de la creació d’espais de trobada multidisciplinaris.',
     },
     {
-      title: 'Psicoteràpia individual per nens, adolescents i adults.',
+      title: 'Supervisions Clíniques',
       description:
-        'La psicoteràpia individual de nens, adolescents i adults s’inicia amb un procés diagnòstic previ on identificarem l’origen del conflicte i farem una proposta de tractament personalitzada amb la finalitat de proporcionar a la persona tant orientació com recursos per solucionar-ho.',
+        'La supervisió clínica és un dels eixos fonamentals de NOMAD, on oferim espais de supervisió grupal o individual per a professionals de la salut, amb l’objectiu de treballar casos clínics i promoure el creixement personal i professional.',
     },
   ]
 
--- src/components/Portada.svelte
+++ src/components/Portada.svelte
@@ -1,43 +1,66 @@
 <script lang="ts">
-    import Svg from "./details/Svg.svelte";
+  import Svg from './details/Svg.svelte'
 </script>
 
 <style lang="scss">
-    .portada-content {
-        position: relative;
-        width: 100%;
-        height: 55dvh;
-        background-color: var(--colorBrand);
+  .portada-content {
+    position: relative;
+    width: 100%;
+    height: 70dvh;
+    background-color: var(--colorBrand);
+    overflow: hidden;
 
-        .coneixens {
-            position: absolute;
-            bottom: 20px;
-            right: 0;
-            left: 0;
-            margin: 0 auto;
-
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 5px;
-
-            span {
-                color: white;
-                font-weight: 100;
-                font-size: 13px;
-            }
-                
-            :global(svg) {
-                transform: rotate(-90deg);
-            }
-        }
+    .logo {
+      position: absolute;
+      top: 15%;
+      left: 0;
+      right: 0;
+      margin: auto;
+      height: 70%;
+      opacity: 0.8;
+      z-index: 2;
     }
+
+    .linea {
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin: auto;
+      width: 95%;
+      top: -20%;
+      z-index: 0;
+    }
+
+    .coneixens {
+      position: absolute;
+      bottom: 20px;
+      right: 0;
+      left: 0;
+      margin: 0 auto;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 5px;
+
+      span {
+        color: white;
+        font-weight: 100;
+        font-size: 13px;
+      }
+
+      :global(svg) {
+        transform: rotate(-90deg);
+      }
+    }
+  }
 </style>
 
 <div class="portada-content">
-    <div class="logo"></div>
-    <div class="coneixens">
-        <span>Coneixe'ns</span>
-        <Svg name="arrow2" fill="white"/>
-    </div>
+  <img class="logo" src="/assets/cara_logo.svg" alt="logo" />
+  <img class="linea" src="/assets/linea.svg" alt="linea" />
+  <div class="coneixens">
+    <span>Coneixe'ns</span>
+    <Svg name="arrow2" fill="white" />
+  </div>
 </div>
