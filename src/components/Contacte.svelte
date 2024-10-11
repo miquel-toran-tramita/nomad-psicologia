cfc517d4945877aa3e58021f24f376347bc81d08
Scripted <miquel@tramita.info>
2024-10-11 20:23:58 +0200

    contacto done

--- src/components/Contacte.svelte
+++ src/components/Contacte.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
   import Svg from '@/components/details/Svg.svelte'
+
+  let sended: boolean = false
 </script>
 
 <style lang="scss">
   @import 'src/sass/mixins.scss';
 
   .contact-container {
-    padding: 50px 0;
+    padding-top: 50px;
+    padding-bottom: 50px;
 
     .content {
       display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
       gap: 80px;
 
       .form {
@@ -23,16 +26,27 @@
           input,
           textarea {
             min-height: 50px;
-            border: 1px solid lightgray;
+            border: 1px solid #bebebe;
             border-radius: 5px;
+            padding: 10px;
+            font-size: 16px;
+            outline: none;
+          }
+
+          textarea {
+            height: 100px;
+            resize: vertical;
           }
           &.radio {
-            padding-top: 0;
+            padding-top: 20px;
+            padding-bottom: 30px;
             flex-direction: row;
+
             input {
               width: 25px;
               height: 25px;
-              border: 1px solid lightgray !important;
+              min-height: 0;
+              border-radius: 100%;
             }
 
             align-items: center;
@@ -89,31 +103,30 @@
 
       <div class="input-group">
         <label for="">Email</label>
-        <input type="text" name="Telèfon" required />
+        <input type="text" name="Email" required />
       </div>
 
       <div class="input-group">
         <label for="">Telèfon</label>
-        <input type="email" name="Email" required />
+        <input type="text" name="Telèfon" required />
       </div>
 
       <div class="input-group">
         <label for="">Deixa el teu comentario</label>
-        <textarea name="Missatge" required></textarea>
+        <textarea name="Missatge"></textarea>
       </div>
 
       <div class="input-group radio">
-        <input type="radio" />
+        <input type="checkbox" required />
         <label for="">He llegit i acepto la <a href="/politica-de-privacitat">Política de privacitat</a></label>
       </div>
 
       <!-- Honeypot Spam Protection -->
       <input type="checkbox" name="botcheck" class="hidden" style="display: none;" />
 
-      <!-- Custom Confirmation / Success Page -->
-      <input type="hidden" name="redirect" value="/gracies" />
+      <input type="hidden" name="redirect" value="http://localhost:4000/" />
 
-      <button type="submit" class="g-btn">ENVIAR</button>
+      <button type="submit" class="g-btn" on:click={() => (sended = true)}>ENVIAR</button>
     </form>
 
     <div class="info">
